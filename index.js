8633e2c726a8bbe8384e77761f8b8a1a71520278
Eldri <rexhepieldri@gmail.com>
2025-06-04 15:23:27 +0200

    improved chrome-extension

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
-
+const ulEl = document.getElementById("ul-el")
+let listItems = ""
 
 inputBtn.addEventListener("click", saveLead);
 function saveLead(){
@@ -10,4 +11,25 @@ function saveLead(){
 console.log(myLeads)
 
 
+for(let i = 0; i < myLeads.length; i++){
+   listItems += "<li>" + myLeads[i] + "</li>"
+   console.log(listItems)
+   //method1
+   //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+   //method2
+   // const li = document.createElement("li")
+   // li.textContent = myLeads[i]
+   // ulEl.append(li)
+}
+ulEl.innerHTML = listItems
+// method 2
+// inputBtn.addEventListener("click", function(){
+//     myLeads.push(inputEl.value)
+// });
+
+// method 3
+// inputBtn.addEventListener("click", () =>
+// {
+//  myLeads.push(inputEl.value)
+// } );
  
