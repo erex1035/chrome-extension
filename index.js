969abda5b2de1553ac1f0bb6afc7d65769099eb9
erex1035 <rexhepieldri@gmail.com>
2025-06-05 15:07:45 +0200

    improved the code

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
-let listItems = ""
 
 inputBtn.addEventListener("click", saveLead);
 function saveLead(){
+   if (inputEl.value === ""){
+      inputEl.placeholder = "Please write something"
+   }
+   else{
    myLeads.push(inputEl.value)
+   inputEl.value = ""
+   renderLeads()
+   }
 }
-console.log(myLeads)
-
-
-for(let i = 0; i < myLeads.length; i++){
-   listItems += "<li>" + myLeads[i] + "</li>"
-   console.log(listItems)
-   //method1
-   //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-   //method2
-   // const li = document.createElement("li")
-   // li.textContent = myLeads[i]
-   // ulEl.append(li)
+function renderLeads() {
+   let listItems = ""
+   for(let i = 0; i < myLeads.length; i++){
+   //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+     listItems += 
+     `<li>
+         <a href="${myLeads[i]}" target="_blank">
+            ${myLeads[i]}
+         </a>
+      </li>`
+   }
+   ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
-// method 2
-// inputBtn.addEventListener("click", function(){
-//     myLeads.push(inputEl.value)
-// });
 
-// method 3
-// inputBtn.addEventListener("click", () =>
-// {
-//  myLeads.push(inputEl.value)
-// } );
- 
